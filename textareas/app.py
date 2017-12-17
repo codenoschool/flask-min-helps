2daaa711f68387d5685daebce73238160d2d8556
codenoschool <codenoschool@gmail.com>
2017-12-16 20:27:17 -0600

    TextAreas (save text to DB)
    Handle the text of your textareas and save it into your database.
    
    Now you can show that info in your templates correctly changin the line
    jumps with <br> tags.
    
    @codenoschool

---
+++ textareas/app.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+import os
+
+DBURI = "sqlite:///" + os.path.abspath(os.getcwd()) + "/database.db"
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = DBURI
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    document = db.Column(db.Text(length=None), nullable=False)
+
+@app.route("/")
+def index():
+
+    return "Hello World!"
+
+@app.route("/add/document", methods=["GET", "POST"])
+def document():
+    if request.method == "POST":
+        new_post = Post(document=request.form["document"])
+        db.session.add(new_post)
+        db.session.commit()
+
+        return "Document saved."
+
+    return render_template("add_document.html")
+
+@app.route("/document/<int:id>")
+def get_document(id):
+
+    post = Post.query.get(id)
+    bodytoHTML = post.document.replace("\n", "<br>")
+
+    return render_template("document.html", post=post, body=bodytoHTML)
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(debug=True)
